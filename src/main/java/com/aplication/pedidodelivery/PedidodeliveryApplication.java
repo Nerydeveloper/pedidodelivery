5695656a8fb6618ba0c91ef643471f5945aeb1b3
Nery Developer <nerydevelopercoder@gmail.com>
2024-08-26 09:36:06 -0300

    Initial commit

---
+++ src/main/java/com/aplication/pedidodelivery/PedidodeliveryApplication.java
@@ -0,0 +1,13 @@
+package com.aplication.pedidodelivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PedidodeliveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PedidodeliveryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aplication/pedidodelivery/PedidodeliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aplication.pedidodelivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PedidodeliveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
