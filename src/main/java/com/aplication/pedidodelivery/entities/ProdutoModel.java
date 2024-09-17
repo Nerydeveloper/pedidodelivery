b4aecad347f221ccb866b951ff9ab0670433440c
Nery Developer <nerydevelopercoder@gmail.com>
2024-09-17 10:23:57 -0300

    "Estrutura Usuario e Produto"
    This reverts commit c106375db1196f5e91692cb9513f22063095d767.

---
+++ src/main/java/com/aplication/pedidodelivery/entities/ProdutoModel.java
@@ -0,0 +1,32 @@
+package com.aplication.pedidodelivery.entities;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "tb_produtos")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProdutoModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long codigoProduto;
+
+    @Column(name = "nomeProduto", nullable = false, length = 100)
+    private String nomeProduto;
+    @Column(name = "descricaoProduto", length = 255)
+    private String descricaoProduto;
+    @Column(name = "precoProduto", nullable = false)
+    private Double precoProduto;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "dataCadastro")
+    private Date dataCadastro;
+}
