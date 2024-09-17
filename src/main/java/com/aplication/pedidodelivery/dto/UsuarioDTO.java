c106375db1196f5e91692cb9513f22063095d767
Nery Developer <nerydevelopercoder@gmail.com>
2024-09-17 10:21:06 -0300

    Revert "Estrutura Usuario e Produto"
    This reverts commit 5a59ba9db35a74e9bbdb3f2ba95207da9ee7435d.

--- src/main/java/com/aplication/pedidodelivery/dto/UsuarioDTO.java
+++
@@ -1,8 +0,0 @@
-package com.aplication.pedidodelivery.dto;
-
-public record UsuarioDTO(String nome,
-                         String email,
-                         String senha,
-                         String endereco,
-                         String telefone) {
-}
